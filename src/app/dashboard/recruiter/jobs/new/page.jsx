2cd08718fdf464508b909b9f0d6e6ee41135ffc6
Sree Nayon Kumar Pal <nayon.cse55@gmail.com>
2026-06-07 13:58:15 +0600

    Some page created

---
+++ src/app/dashboard/recruiter/jobs/new/page.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NewJob = () => {
+  return (
+    <div>
+      <h3>Create New Job</h3>
+    </div>
+  );
+};
+
+export default NewJob;
---
+++ src/app/dashboard/recruiter/jobs/page.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const RecruiterJobs = () => {
+  return (
+    <div>
+      <h3>Recruiter/Company All Jobs </h3>
+    </div>
+  );
+};
+
+export default RecruiterJobs;
