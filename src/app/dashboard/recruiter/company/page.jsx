ff9e040a5187236dafb789efc503cabe44c94525
Sree Nayon Kumar Pal <nayon.cse55@gmail.com>
2026-07-04 00:39:50 +0600

    Company profile design started

--- src/app/dashboard/recruiter/company/page.jsx
+++ src/app/dashboard/recruiter/company/page.jsx
@@ -1,9 +1,18 @@
 import React from "react";
+import CompanyProfile from "./CompanyProfile";
+import { getUserSession } from "@/lib/core/session";
+import { getRecruiterCompany } from "@/lib/api/companies";
 
-const CompanyPage = () => {
+const CompanyPage = async () => {
+  const user = await getUserSession();
+  const company = await getRecruiterCompany(user?.id);
+
   return (
     <div>
-      <p>CompanyPage</p>
+      <CompanyProfile
+        recruiter={user}
+        recruiterCompany={company}
+      ></CompanyProfile>
     </div>
   );
 };
