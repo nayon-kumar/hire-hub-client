a8ff69af263bcfd23a3207a025b282c5ad74d93f
Sree Nayon Kumar Pal <nayon.cse55@gmail.com>
2026-07-03 21:48:52 +0600

    Job post is working

--- src/lib/actions/jobs.js
+++ src/lib/actions/jobs.js
@@ -2,6 +2,13 @@
 
 const baseURL = process.env.NEXT_PUBLIC_BASE_URL;
 
-export const getJobs = async (newJobData) => {
-  const res = await fetch(`${baseURL}/api/jobs`);
+export const createJob = async (newJobData) => {
+  const res = await fetch(`${baseURL}/api/jobs`, {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json",
+    },
+    body: JSON.stringify(newJobData),
+  });
+  return res.json();
 };
