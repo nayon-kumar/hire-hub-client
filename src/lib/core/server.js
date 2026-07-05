b9d54c018783103b9cb9c5f778ccc4df8d7a877b
Sree Nayon Kumar Pal <nayon.cse55@gmail.com>
2026-07-05 15:54:40 +0600

    Job post is working properly

--- src/lib/core/server.js
+++ src/lib/core/server.js
@@ -1,5 +1,17 @@
 const baseURL = process.env.NEXT_PUBLIC_BASE_URL;
 
+export const serverFetch = async (path) => {
+  const res = await fetch(`${baseURL}${path}`);
+
+  if (!res.ok) {
+    const text = await res.text();
+    console.error(text);
+    throw new Error(`Request failed: ${res.status}`);
+  }
+
+  return res.json();
+};
+
 export const serverMutation = async (path, data) => {
   const res = await fetch(`${baseURL}${path}`, {
     method: "POST",
