d88623446d1e1351ca23c158c1d152ffdc072fc4
Sree Nayon Kumar Pal <nayon.cse55@gmail.com>
2026-07-18 22:28:36 +0600

    Admin companies page token added

--- src/lib/core/server.js
+++ src/lib/core/server.js
@@ -1,8 +1,27 @@
+import { getUserToken } from "./session";
+
 const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
+
+export const authHeader = async () => {
+  const token = await getUserToken();
+  const header = token
+    ? {
+        authorization: `Bearer ${token}`,
+      }
+    : {};
+  return header;
+};
 
 export const serverFetch = async (path) => {
   const res = await fetch(`${baseUrl}${path}`);
   // handle 401, 404, 403
+  return res.json();
+};
+
+export const protectedFetch = async (path) => {
+  const res = await fetch(`${baseUrl}${path}`, {
+    headers: await authHeader(),
+  });
   return res.json();
 };
 
@@ -11,6 +30,7 @@ export const serverMutation = async (path, data, method = "POST") => {
     method: method,
     headers: {
       "Content-Type": "application/json",
+      ...(await authHeader()),
     },
     body: JSON.stringify(data),
   });
